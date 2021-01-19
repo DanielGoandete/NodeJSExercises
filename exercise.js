ee5f2fc7a3c34ad98d371228f3ba63c4d2942b88
DanielGoandete <63291047+DanielGoandete@users.noreply.github.com>
2021-01-19 13:46:34 +0100

    exercise

--- exercise.js
+++ exercise.js
@@ -27,8 +27,31 @@ function excercise1() {
 // Bonus 2: be polite in the error messages. (facultative)
   function excercise2() {
 
-    alert('donee');
-    
+    var min = document.getElementById("min");
+    var max = document.getElementById("max");
+    var current = document.getElementById("current");
+    var strMin = 'min';
+    var strMax = 'max';
+    var strCurrent = 'current';
+    var minWithoutString = strMin.replace(/\D/g, "");
+    var maxWithoutString = strMax.replace(/\D/g, "");
+    var CurrentWithoutString = strCurrent.replace(/\D/g, "");
+    alert(current.value);
+    if(min.value != minWithoutString || max.value != maxWithoutString || current.value != currentWithoutString){
+        if(min.value > max.value || min.value > current.value){
+            alert('Sorry, min is greater than current or max try again');
+            
+        } 
+        if(current.value > max.value || current.value < min.values){
+            alert('your current value is not correct, try again');
+            
+        }
+        if(min.value < max.value && current.value > min.value && current.value < max.value){
+            alert('Perfect');
+        }
+    }else {
+        alert('Sorry, you must input some number only');
+    }  
   }
 
 
@@ -37,8 +60,14 @@ function excercise1() {
 //  The second one should use for and make a loop that will execute 50 times
   function excercise3() {
 
-    alert('donee');
-    
+    var count = 100;
+    var start = 1
+    for(var i = start; i <= count; i++){
+        if(i % 2 == 0){
+            alert(i + " ");
+        }    
+    }
+  
   }
 
 
@@ -46,7 +75,19 @@ function excercise1() {
 //    For every odd number display the result of their multiplication by 3.
   function excercise4() {
 
-    alert('donee');
+    var count = 100;
+    var start = 1
+    var countResultBy2 = 0;
+    var countResultBy3 = 0;
+    for(var i = start; i <= count; i++){
+        if(i % 2 == 0){
+            countResultBy2 += i /2;
+            alert( countResultBy2);
+        } else{
+            countResultBy3 += i *3;
+            alert( countResultBy3);
+        }      
+    }
     
   }
 
@@ -54,8 +95,20 @@ function excercise1() {
 //   Make a program that asks the favorite number of the user. If that number is anything other than 42 display "Are you sure?" and ask again. 
 //   (This program should never end as long as the user didn't chose 42).
   function excercise5() {
-
-    alert('donee');
+     var number;
+    var choosedNumber = document.getElementById("number").value;
+    if(choosedNumber == 42){   
+        alert('WOW you got it on the first time');
+    }else{
+        while(choosedNumber != 42){
+            var luckOK = prompt("Please guess my luck number");
+            if(luckOK == 42){
+                alert('WOW you got it');
+                choosedNumber = luckOK;
+            }  
+        }
+    }
+     
     
   }
 
@@ -63,7 +116,21 @@ function excercise1() {
 //    Depending on the number, display the day of the week that correspond. (1 => Monday, 2 => Tuesday, etc...)
   function excercise6() {
 
-    alert('donee');
+    var choosedNumber = document.getElementById("numberWeek").value;
+    var dayOfWeek = ['null','monday','tuesday','wednesday','thrusday','friday','saturday','sunday'];
+    var i ;
+    if(choosedNumber < 1 || choosedNumber > 7 ){   
+        alert('Sorry this is not a good number, please put between 1 and 7.');
+    }else{
+        
+        for (i = 0; i < dayOfWeek.length; i++) {
+            if(dayOfWeek[choosedNumber] == dayOfWeek[i]){
+                alert('today is: '+ dayOfWeek[i]);
+                break;
+            }
+          }
+    }
+    
     
   }
 
